ba86eaff58e6746970d110702d9e58c65ad92783
Carlos Araújo <carlos.araujo@concrete.com.br>
2019-05-22 18:39:29 -0300

    Detalhes do cliente e schema

--- schemas/schema_journey.js
+++ schemas/schema_journey.js
@@ -10,7 +10,48 @@ const schemaLogin = Joi.object({
     }),
 });
 
+const schemaArrayClients = Joi.object({
+  id: Joi.number().required().options({
+    convert: false
+  }),
+  name: Joi.string().required(), 
+  age: Joi.number().required().options({
+    convert: false  
+  }),
+});
+
+const schemaClients = Joi.object({
+  clients: Joi.array().items(schemaArrayClients),
+  timestamp: Joi.string().optional(), 
+  page: Joi.number().required().optional({
+      convert: false
+  }
+  ),
+});
+
+const schemaObjPhone = Joi.object({
+  type: Joi.string().optional(),
+  number: Joi.number().required().required({
+    convert: false
+  }),
+});
+
+const schemaDetails = Joi.object({
+  id: Joi.number().required().optional({
+    convert: false
+  }),
+  name: Joi.string().optional(),
+  age: Joi.number().required().required({
+    convert: false
+  }),
+  email: Joi.string().optional(),
+  is_active: Joi.boolean().required(),
+  phone: schemaObjPhone,
+  debit_alert: Joi.boolean().allow(null).optional(),
+});
 
 module.exports = {
     schemaLogin,
-  };
+    schemaClients,
+    schemaDetails,
+};
--- tests/journey_test.js
+++ tests/journey_test.js
@@ -6,7 +6,9 @@ var request = require("supertest"),
 
 
 const {
-    schemaLogin,  
+    schemaLogin,
+    schemaClients, 
+    schemaDetails, 
   } = require("../schemas/schema_journey.js");
 
 const request_timeout = 10000;
@@ -14,9 +16,12 @@ const user = {
     name: "darling",
     password: 12345
 };
+
+let id = '';
 let token = ''; 
 const URL = process.env.NODE_ENV;
 const PATH_LOGIN = '/login';
+const PATH_CLIENTS = '/clients';
 
 describe("Testes de Contrato", function() {
     it("Realizar Login",function(done) {
@@ -30,5 +35,30 @@ describe("Testes de Contrato", function() {
         joiAssert(res.body, schemaLogin);
         done(err);
         });
+    });
+
+    it("Listar clientes", function(done) {
+        this.timeout(request_timeout);
+        request(URL)
+        .get(PATH_CLIENTS)
+        .expect("Content-type", /json/)
+        .end(function(err, res) {
+            expect(res.status).to.be.eql(200);
+            joiAssert(res.body, schemaClients);
+            id = res.body.clients[0].id
+            done(err);
+        });
+    });
+
+    it("Detalhes do cliente", function (done) {
+        this.timeout(request_timeout);
+        request(URL)
+            .get(PATH_CLIENTS + '/' + id)
+            .expect("Content-type", /json/)
+            .end(function (err, res) {
+                expect(res.status).to.be.eql(200);
+                joiAssert(res.body, schemaDetails);
+                done(err);
+            });
     });
 })
